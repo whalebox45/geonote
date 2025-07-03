fd5c32258a93165605ae7bee72cf1182316621ca
whale <whalebox4.5@gmail.com>
2025-07-04 04:41:23 +0800

    feat: migrate Compose configuration for backend, MongoDB, and test services

--- mongo_docker.ps1
+++ mongo_docker.ps1
@@ -1,4 +1,6 @@
 # docker run --name mongo -d -p 27017:27017 -v mongodata:/data/db mongo
 
-docker-compose up --build
+# docker-compose up --build
 # docker-compose up -d --build
+
+podman compose up 
